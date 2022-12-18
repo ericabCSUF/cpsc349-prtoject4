f9abf9778675a68fc38d83f5d459694bf44552be
Erica <ericab90@csu.fullerton.edu>
2022-12-17 16:25:56 -0800

    done

--- pb_public/app.js
+++ pb_public/app.js
@@ -1,11 +1,11 @@
 const pb = new PocketBase("http://127.0.0.1:8090");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const records = await pb.collection("posts").getFullList(200, {
-  sort: "+created"
+  sort: "-created"
 });
 console.log(records);
 function Postlist() {
-  return records.map((r) => /* @__PURE__ */ React.createElement("div", { className: "checklist" }, /* @__PURE__ */ React.createElement("input", { type: "checkbox", id: r.id }), /* @__PURE__ */ React.createElement("span", null, r.body), /* @__PURE__ */ React.createElement("button", { onClick: () => removeTODO(r.id) }, "Remove ")));
+  return records.map((r) => /* @__PURE__ */ React.createElement("div", { className: "checklist" }, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("input", { type: "checkbox", id: r.id })), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("span", null, r.body)), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("button", { onClick: () => removeTODO(r.id) }, "Remove "))));
 }
 async function Addnewtodo(input) {
   const data = {
@@ -29,5 +29,5 @@ export default function App() {
   async function Logoutclick() {
     pb.authStore.clear();
   }
-  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", { className: "py-16" }, /* @__PURE__ */ React.createElement("h1", { className: " text-7xl text-center text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" }, " Welcome to your todo list ")), /* @__PURE__ */ React.createElement("div", { className: "todoPrompt" }, /* @__PURE__ */ React.createElement("input", { type: "text", id: "userInput", placeholder: "Add post here" }), /* @__PURE__ */ React.createElement("button", { className: "btn", onClick: othername }, "Add TODO ")), /* @__PURE__ */ React.createElement("div", { className: "container" }, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Postlist, null)), /* @__PURE__ */ React.createElement("br", null)));
+  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("div", { className: "title" }, /* @__PURE__ */ React.createElement("h1", { className: "text-decoration-underline text-7xl text-center text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" }, " Welcome to your todo list "), /* @__PURE__ */ React.createElement("h2", { className: "text-center" }, "Type your task below and click Add Task to add it to your To-Do List. To remove a task, simply click the Remove button beside it"), /* @__PURE__ */ React.createElement("br", null)), /* @__PURE__ */ React.createElement("div", { className: "container1" }, /* @__PURE__ */ React.createElement("div", { className: "todoPrompt" }, /* @__PURE__ */ React.createElement("input", { type: "text", id: "userInput", placeholder: "Add post here" }), /* @__PURE__ */ React.createElement("button", { className: "btn", onClick: othername }, "Add Task ")), /* @__PURE__ */ React.createElement("div", { className: "container2" }, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement(Postlist, null)), /* @__PURE__ */ React.createElement("br", null))));
 }
--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,26 @@
-
 const pb = new PocketBase('http://127.0.0.1:8090')
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 // you can also fetch all records at once via getFullList
 const records = await pb.collection('posts').getFullList(200 /* batch size */, {
-    sort: '+created',
+    sort: '-created',
 });
 console.log(records)
-
 function Postlist(){
   return (
     records.map(r => (
       <div className="checklist">
-        <input type="checkbox" id = {r.id} />
-        <span >
-          {r.body}
-        </span>
-        <button onClick={() => removeTODO(r.id)}>Remove </button>
+        <div>
+          <input type="checkbox" id = {r.id} />
+        </div>
+        <div>
+          <span >
+            {r.body}
+          </span>
+        </div>
+        <div>
+          <button onClick={() => removeTODO(r.id)}>Remove </button>
+        </div>
       </div>
     ))
   );
@@ -41,29 +45,29 @@ root.render(
     <App />
   </React.StrictMode>
 )
-
 export default function App () {
   async function Logoutclick(){
     pb.authStore.clear();
   }
   return (
     <div>
-      <div className="py-16">
-        <h1 className=" text-7xl text-center text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"> Welcome to your todo list </h1>
-
-      </div>
-      <div className="todoPrompt">
-      <input type="text" id="userInput" placeholder="Add post here"/>
-      <button className="btn" onClick= {othername} >Add TODO </button>
-      </div>
-      <div className="container">
-
-        <div>
-
-
-          <Postlist/>
-        </div>
+      <div className="title">
+        <h1 className="text-decoration-underline text-7xl text-center text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"> Welcome to your todo list </h1>
+        <h2 className="text-center">Type your task below and click Add Task to add it to your To-Do List. To remove a task, simply click the Remove button beside it</h2>
         <br></br>
+      </div>
+      <div className="container1">
+        <div className="todoPrompt">
+          <input type="text" id="userInput" placeholder="Add post here"/>
+          <button className="btn" onClick= {othername} >Add Task </button>
+        </div>
+        <div className="container2">
+          <div>
+            <br></br>
+            <Postlist/>
+          </div>
+          <br></br>
+        </div>
       </div>
     </div>
   );
